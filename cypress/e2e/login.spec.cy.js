a0a08b90499e429879caa0ae29d0f0f3877ceda7
Larissa Tonetto <larigottschall@hotmail.com>
2022-07-06 16:09:56 -0300

    login com fixture e log false

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -22,13 +22,26 @@ context('Funcionalidade Login', () => {
     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá')
   })
 
-  it.only('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+  it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
     cy.get('#username').type(perfil.usuario)
     cy.get('#password').type(perfil.senha)
     cy.get('.woocommerce-form > .button').click()
 
     cy.get('.page-title').should('contain', 'Minha conta')
     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá')
+
+  });
+
+  it.only('Deve fazer login com sucesso - Usando fixture', () => {
+    cy.fixture('perfil').then(dados => {
+      cy.get('#username').type(dados.usuario)
+      cy.get('#password').type(dados.senha, {log: false})
+      cy.get('.woocommerce-form > .button').click()
+  
+      cy.get('.page-title').should('contain', 'Minha conta')
+      cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá')
+
+    })
 
   });
 
