95ff12065eed685667849027b72cc12b545220e2
Larissa Tonetto <larigottschall@hotmail.com>
2022-07-06 15:47:26 -0300

    alterações com baseurl

--- cypress/e2e/pre-cadastro.spec.cy.js
+++ cypress/e2e/pre-cadastro.spec.cy.js
@@ -5,7 +5,7 @@ var faker = require('faker');
 describe ('Funcionalidade Pré-cadastro', () =>{
 
     beforeEach ( () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     
       });
 
