6f688ea24e4117bae69afd4c820753743fbd15d0
Larissa Tonetto <larigottschall@hotmail.com>
2022-07-06 15:59:01 -0300

    login com arquivo de dados

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,41 +1,52 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
-context ('Funcionalidade Login', () =>{
+context('Funcionalidade Login', () => {
 
-    beforeEach ( () => {
-      cy.visit('minha-conta')
-  
-    });
-  
-    afterEach ( () => {
-      cy.screenshot ()
-  
-    });
-  
-    it('Deve fazer login com sucesso' , () => {
-        cy.get('#username').type('aluno_ebac@teste.com')
-        cy.get('#password').type('teste@teste.com')
-        cy.get('.woocommerce-form > .button').click()
-      
-        cy.get('.page-title').should ('contain' , 'Minha conta')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should ('contain' , 'Olá')
-    })
-  
-    it('Deve exibir uma mensagem de erro ao inserir usuário inválido' , () => {
-        cy.get('#username').type('ebac@teste.com')
-        cy.get('#password').type('teste@teste.com')
-        cy.get('.woocommerce-form > .button').click()
-  
-        cy.get('.woocommerce-error > li').should ('contain' , 'A senha fornecida para o e-mail')
-        
-    })
-  
-    it('Deve exibir uma mensagem de erro ao inserir senha inválida' , () => {
-        cy.get('#username').type('aluno_ebac@teste.com')
-        cy.get('#password').type('teste@teste')
-        cy.get('.woocommerce-form > .button').click()
-    
-        cy.get('.woocommerce-error > li').should('contain' , 'Erro: A senha fornecida')
-    })
-  
+  beforeEach(() => {
+    cy.visit('minha-conta')
+
+  });
+
+  afterEach(() => {
+    cy.screenshot()
+
+  });
+
+  it('Deve fazer login com sucesso', () => {
+    cy.get('#username').type('aluno_ebac@teste.com')
+    cy.get('#password').type('teste@teste.com')
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.page-title').should('contain', 'Minha conta')
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá')
   })
+
+  it.only('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+    cy.get('#username').type(perfil.usuario)
+    cy.get('#password').type(perfil.senha)
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.page-title').should('contain', 'Minha conta')
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá')
+
+  });
+
+  it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+    cy.get('#username').type('ebac@teste.com')
+    cy.get('#password').type('teste@teste.com')
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.woocommerce-error > li').should('contain', 'A senha fornecida para o e-mail')
+
+  })
+
+  it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+    cy.get('#username').type('aluno_ebac@teste.com')
+    cy.get('#password').type('teste@teste')
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida')
+  })
+
+})
