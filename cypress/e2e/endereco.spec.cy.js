6f30f6c65a0203ea70b412b83950ca35946157db
Larissa Tonetto <larigottschall@hotmail.com>
2022-07-07 19:22:11 -0300

    usando arquivo de dados

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,8 +11,25 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
     });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
         EnderecoPage.editarEnderecoFaturamento('Ana', 'Banana', 'Google', 'Brasil', 'Rua Bananal', '123', 'Santos', 'São Paulo', '11222333', '11987654321', 'banana@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].rua,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].celular,
+            dadosEndereco[1].email
+            )
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 
